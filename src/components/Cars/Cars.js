2b9f3bdd370669607f310193901266824653e8c0
Augusto Sacramento Rodrigues <ra213368@lopes.lab.ic.unicamp.br>
2022-10-20 14:58:34 -0300

    cars v1.0

--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import PropTypes from "prop-types";
+import { Navigate } from 'react-router-dom';
 
 export default function Cars() {
     return( 
-        <h2>Cars</h2>
+        <div class='container border'>
+
+            <div class='row border'>
+               <div class='col-4'>
+                    <img src='https://imgur.com/a/v4qwgmp' alt='carro'/>
+               </div>
+               <div class="col-8">
+                   <p>Modelo: Fiat Uno</p>
+                   <p>Placa:XXXXXXXX</p>
+                   <p>Ano: 2016</p>
+               </div>
+            </div>    
+            <div class='row border'>
+                <h2>2</h2>
+            </div>   
+            <div class='row border'>
+                <h2>3</h2>
+            </div>   
+            
+        </div>
+        
     )
 }
