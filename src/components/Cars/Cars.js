b51873d11813c5b94cc7a9c312a480aee2786806
Augusto Sacramento Rodrigues <ra213368@nunes.lab.ic.unicamp.br>
2022-11-08 17:19:33 -0300

    cars v3

--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -4,6 +4,8 @@ import Form from 'react-bootstrap/Form';
 import PropTypes from "prop-types";
 import { Navigate } from 'react-router-dom';
 
+
+
 export default function Cars() {
 
     async function loginUser(credentials) {
@@ -16,28 +18,52 @@ export default function Cars() {
         })).then(data => data.json());
         
     }
+    const cars = [{modelo:"Fiat Uno",placa:"ABC1234",ano:"2016",cor:"Preto"},
+    {modelo:"Corsa",placa:"DEF5678",ano:"2020",cor:"Prata"},
+    {modelo:"Onix",placa:"GHI9101",ano:"2021",cor:"Preto"}];
+    
+
+
+    //  function dashboard (){
+    //     const cars = [{modelo:"Fiat Uno",placa:"ABC1234",ano:"2016",cor:"Preto"},
+    //             {modelo:"Corsa",placa:"DEF5678",ano:"2020",cor:"Prata"},
+    //             {modelo:"Onix",placa:"GHI9101",ano:"2021",cor:"Preto"}];
+    //     return(
+    //         <div>
+    //             {this.cars.map(car => <div key={car}>car.modelo </div>)}
+    //         </div>
+    //     )
+
+    // }
 
     return( 
-        <div class='container border'>
-
-            <div class='row border'>
-               <div class='col-4'>
-                    <img src='download.jpeg' alt='carro' /> 
-               </div>
-               <div class="col-8">
-                   <p>Modelo: Fiat Uno</p>
-                   <p>Placa:XXXXXXXX</p>
-                   <p>Ano: 2016</p>
-               </div>
-            </div>    
-            <div class='row border'>
-                <h2>2</h2>
-            </div>   
-            <div class='row border'>
-                <h2>3</h2>
-            </div>   
+        <div class='container col-2' >
             
+                 {cars.map(car => <div class='row' key={car}> <div class="border square rounded-pill "><p>Modelo: {car.modelo}</p>
+                                                                     <p>Placa:{car.placa}</p>
+                                                                     <p>Ano: {car.ano}</p>
+                                                                     <p>Cor:{car.cor}</p> </div></div>)}
         </div>
+       
+
+        // <div class='container border col-2'>
+
+        //     <div class='row border'>
+               
+        //        <div class="col-8 ">
+        //            <p>Modelo: Fiat Uno</p>
+        //            <p>Placa:XXXXXXXX</p>
+        //            <p>Ano: 2016</p>
+        //        </div>
+        //     </div>    
+        //     <div class='row border'>
+        //         <h2>2</h2>
+        //     </div>   
+        //     <div class='row border'>
+        //         <h2>3</h2>
+        //     </div>   
+            
+        // </div>
         
     )
 }
